1e79d714427ac63614caa67f794ff62691ddcf0e
Wayne Carter <wtcarter@gmail.com>
2018-04-13 12:18:26 +1200

    Done.

--- cipher.rb
+++ cipher.rb
@@ -3,7 +3,7 @@ cipher         = 'oephjizkxdawubnytvfglqsrcm'
 encrypted_text = 'knlfgnb, sj koqj o yvnewju'
 
 def decrypt(s, cipher)
-  
+  # Uses substitution cipher to decripts given string
   s.chars.each { |ch| print(!!(c = cipher.index(ch)) ? CONSTANT[c] : ch ) }
 end
 
